8509aa6362dfd64a5237c35047cf133a6d00cf92
Bob Hemphill <bob.hemphill@gmail.com>
2011-10-23 14:47:25 -0400

    Added NUnit as an external tool

--- DomainLayerTests/Program.cs
+++ DomainLayerTests/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 namespace DomainLayerTests {
   class Program {
     static void Main(string[] args) {
+      Console.WriteLine("press <enter> to continue.");
+      Console.ReadLine();
     }
   }
 }
